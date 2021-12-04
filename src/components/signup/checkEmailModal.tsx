e0c12a79ebf8d6fa57e9f3fc57ee4920f0ae4b5a
verykenny <16845292+verykenny@users.noreply.github.com>
2021-12-04 11:49:33 -0700

    corrected unused refs

--- src/components/signup/checkEmailModal.tsx
+++ src/components/signup/checkEmailModal.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { CheckIcon } from '@heroicons/react/outline';
 import useSession from '../../context/sessionContext';
