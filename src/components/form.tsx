4d48b02c57377573a7883bd327819b3089801e31
verykenny <16845292+verykenny@users.noreply.github.com>
2022-01-03 13:23:08 -0700

    created function to hit /response route for each element with data in the form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -5,30 +5,49 @@ import FormRadio from './formComponents/formRadio';
 import FormText from './formComponents/formText';
 import FormCheckboxes from './formComponents/formCheckboxes';
 import FormNumber from './formComponents/formNumber';
+import { saveForm } from '../utils';
 
 const Form: React.FC<any> = (form) => {
-    const [formData, setFormData] = React.useState(Object.fromEntries(form.elements.map((element: any) => ([element.id, element]))));
+    const [formData, setFormData] = React.useState(
+        Object.fromEntries(
+            form.elements.map((element: any) => [element.id, element])
+        )
+    );
     const [header, setHeader] = React.useState({ noData: true });
 
     useEffect(() => {
         localStorage.setItem('form' + form.name, JSON.stringify(formData));
     }, [formData, form.name]);
 
-
     if (form.elements[0].type === 'Header' && header.noData) {
         setHeader(form.elements[0]);
     }
 
+    const handleFormSubmit = (e: React.SyntheticEvent) => {
+        // TODO: LOADING INDICATOR
+        e.preventDefault();
+        (async () => {
+            try {
+                await saveForm(Object.values(formData));
+                window.location.href = '/forms';
+            } catch (e) {
+                alert(e);
+            }
+        })();
+    };
+
     return (
         <div className="max-w-7xl mx-auto px-4 pt-10 pb-10 sm:px-6 lg:px-8">
             <div className="max-w-3xl mx-auto">
-                <form className="space-y-8 divide-y divide-gray-200">
+                <form
+                    className="space-y-8 divide-y divide-gray-200"
+                    onSubmit={handleFormSubmit}
+                >
                     <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
                         <div>
-                          <FormHeader header={header} name={form.name} />
-                          <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
-                            {Object.values(formData)
-                                .map((element: any) => {
+                            <FormHeader header={header} name={form.name} />
+                            <div className="mt-6 sm:mt-5 space-y-6 sm:space-y-5">
+                                {Object.values(formData).map((element: any) => {
                                     if (element.type === 'Text Area') {
                                         return (
                                             <FormTextArea
@@ -79,11 +98,13 @@ const Form: React.FC<any> = (form) => {
                                     }
                                     return (
                                         <h1 key={element.id}>
-                                          {element.type !== "Header" ? "ERROR!!!!!!  " + element.type : ""}
+                                            {element.type !== 'Header'
+                                                ? 'ERROR!!!!!!  ' + element.type
+                                                : ''}
                                         </h1>
                                     );
                                 })}
-                                </div>
+                            </div>
                         </div>
                     </div>
 
