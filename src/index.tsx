28ab99fc7e521daa9287416212d46ae7dae0ae2e
verykenny <16845292+verykenny@users.noreply.github.com>
2022-01-12 09:09:07 -0700

    removed sessionContext that was unused

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { SessionProvider } from './context/sessionContext';
+
 import { AuthProvider } from './context/authContext';
 
 ReactDOM.render(
     <React.StrictMode>
-      <SessionProvider>
         <AuthProvider>
         <BrowserRouter>
             <App />
         </BrowserRouter>
         </AuthProvider>
-        </SessionProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
